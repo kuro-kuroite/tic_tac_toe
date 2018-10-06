66c4225058c59999321a8d104fb19aac1ee48092
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-10-06 19:43:09 +0900

    chore: ignore the click if player has already won or if the square is filled

--- src/Board.js
+++ src/Board.js
@@ -35,6 +35,10 @@ export default class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if(this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
