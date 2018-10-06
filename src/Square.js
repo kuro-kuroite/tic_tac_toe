5921282570cf1fa6d4ae8b533b75a868f0d7d99b
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-10-06 17:41:28 +0900

    chore: show alerts when clicked on a square

--- src/Square.js
+++ src/Square.js
@@ -4,7 +4,7 @@ import './Square.css';
 export default class Square extends React.Component {
   render() {
     return (
-      <button className='square'>
+      <button className='square' onClick={() => alert('click') }>
         {this.props.value}
       </button>
     );
