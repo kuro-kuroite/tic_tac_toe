4cd7d1012fd1c2d82bbd169dc6b803345cf7e0d3
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-10-06 17:48:11 +0900

    feat: make X appear when clicking a square

--- src/Square.js
+++ src/Square.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Square.css';
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className='square' onClick={() => alert('click') }>
-        {this.props.value}
+      <button className='square' onClick={() => this.setState({value: 'X'}) }>
+        {this.state.value}
       </button>
     );
   }
