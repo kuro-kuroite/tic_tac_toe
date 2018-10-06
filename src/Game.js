9b87e37df261d2cd711b733a89fd90f316b232d3
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-10-06 17:26:18 +0900

    feat: add Starter Code of react tutorial

---
+++ src/Game.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Game.css';
+import Board from './Board';
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <div>{/* status */}</div>
+          <div>{/* TODO */}</div>
+        </div>
+      </div>
+    );
+  }
+}
